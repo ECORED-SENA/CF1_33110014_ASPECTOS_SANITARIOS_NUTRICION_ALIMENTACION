88acbc24d8c6c99160953e1a19e40e80838ccf5d
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-08-19 21:51:17 -0500

    avances

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -21,6 +21,175 @@
     .fondo-color-sistema-g.p-4.py-lg-5
       figure
         img.mx-auto(src="@/assets/curso/temas/32.svg",style="max-width: 1007px;",alt="La figura presenta la función de los hidratos de carbono, grasa, proteínas, minerales y vitaminas en la estructura del cuerpo, la producción de energía y la regulación metabólica. Se pregunta por la posibilidad de reservas corporales de estos nutrientes.")
+    
+    Separador
+
+    #t_3_1.titulo-segundo.color-acento-contenido
+      h2 3.1 Hidratos de carbono
+    
+    .row.align-items-center
+      .col-lg-9
+        .fondo-color-sistema-p.p-4.h-100.align-content-center
+          p.mb-0 Los hidratos de carbono, también conocidos como glúcidos, se clasifican como monosacáridos o azúcares simples, disacáridos o azúcares dobles, y polisacáridos, que son los de mayor complejidad. La función más relevante de los carbohidratos es el suministro de energía.
+      .col-lg-3.order-first.order-lg-last
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/33.svg",style="max-width: 295px;")
+    
+    .titulo-decorativo.my-4
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Azúcares simples
+    
+    p.mb-4 #[b Los monosacáridos] son compuestos que no pueden ser hidrolizados a otros más simples. Dentro de este grupo se encuentran la glucosa y la fructosa.
+
+    .row.justify-content-center.mb-3
+      .col-lg-2.pe-lg-0
+        .fondo-color-sistema-m.p-4.h-100.align-content-center
+          figure
+            img.mx-auto(src="@/assets/curso/temas/34.svg",style="max-width: 125px;")
+      .col-lg-8.ps-lg-0
+        .fondo-color-sistema-q.p-3.h-100.align-content-center 
+          p.mb-3.fw-bold Glucosa
+          p.mb-0 También conocida como dextrosa, es un monosacárido libre que, aunque es el más abundante, se encuentra en pequeñas cantidades. Está presente en frutas dulces como la uva, la mora y la naranja, y en algunos vegetales como el maíz y la zanahoria. Además, la mayoría de los hidratos de carbono se transforman en glucosa tras el proceso de digestión.
+    .row.justify-content-center.mb-3
+      .col-lg-2.pe-lg-0
+        .fondo-color-sistema-m.p-4.h-100.align-content-center
+          figure
+            img.mx-auto(src="@/assets/curso/temas/35.svg",style="max-width: 125px;")
+      .col-lg-8.ps-lg-0
+        .fondo-color-sistema-q.p-3.h-100.align-content-center 
+          p.mb-3.fw-bold Fructosa
+          p.mb-0 Denominada el azúcar de las frutas. Es un azúcar muy soluble que no se cristaliza rápidamente. Es mucho más dulce que el azúcar de caña y se encuentra en la miel, las frutas maduras y en algunos vegetales; también es producto de la hidrólisis de la sacarosa.
+    .row.justify-content-center.mb-3
+      .col-lg-2.pe-lg-0
+        .fondo-color-sistema-m.p-4.h-100.align-content-center
+          figure
+            img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 160px;")
+      .col-lg-8.ps-lg-0
+        .fondo-color-sistema-q.p-3.h-100.align-content-center 
+          p.mb-3.fw-bold Galactosa
+          p.mb-0 Este compuesto no se encuentra libre en ningún alimento; solo se forma a partir de la hidrólisis de la lactosa. Además, es un componente de polisacáridos de los vegetales, como es el caso de las pectinas, gomas y mucílagos.
+
+    .titulo-decorativo.my-4
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Disacáridos
+    
+    p.mb-4 Son azúcares abundantes en los alimentos, los más conocidos son la sacarosa y la lactosa.
+    
+    .fondo-color-primario.borde-20.p-4.overflow-hidden.position-relative.mb-4
+      img.fig-abs-p-1(src="@/assets/curso/temas/37.png")
+      PasosB.pasos-personalizado-1
+        .row.justify-content-center.pt-lg-4(titulo="Lactosa")
+          .col-lg-6.mb-4.mb-lg-0
+            h4.mb-5 Lactosa
+            p.mb-0.me-lg-4 Azúcar de la leche producida por los mamíferos. Es el único carbohidrato de origen animal de gran importancia en la dieta. La concentración en la leche varía del 2% al 8%, dependiendo de la especie del animal.
+          .col-lg-5
+            figure
+              img.mx-auto(src="@/assets/curso/temas/38.png",style="max-width: 400px;")
+        .row.justify-content-center.pt-lg-4(titulo="Sacarosa")
+          .col-lg-6.mb-4.mb-lg-0
+            h4.mb-5 Sacarosa
+            p.mb-0.me-lg-4 Azúcar de mesa más conocido, presente en la caña de azúcar y la remolacha. Su fórmula química está compuesta por una molécula de fructosa y una de glucosa. Se encuentra de forma natural en muchas frutas y se usa en productos de panadería, pastelería y bebidas refrescantes. Su hidrólisis da como resultado el llamado azúcar invertido.
+          .col-lg-5
+            figure
+              img.mx-auto(src="@/assets/curso/temas/39.png",style="max-width: 400px;")
+        .row.justify-content-center.pt-lg-4(titulo="Maltosa")
+          .col-lg-6.mb-4.mb-lg-0
+            h4.mb-5 Maltosa
+            p.mb-0.me-lg-4 Azúcar de la malta, compuesta por dos moléculas de glucosa. Se produce durante la germinación de los granos y está presente en la cerveza y en los cereales malteados.
+          .col-lg-5
+            figure
+              img.mx-auto(src="@/assets/curso/temas/40.png",style="max-width: 400px;")
+    
+    p.fw-bold.mb-5 Los monosacáridos y disacáridos poseen características comunes, tales como:
+
+    .row 
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Absorción rápida y fácil
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Sabor dulce
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Solubilidad en agua y facilidad para formar jarabes
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Capacidad de cristalización
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Valor energético
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Carácter de carbohidratos fermentables
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Capacidad de inhibir el crecimiento de microorganismos en concentraciones elevadas
+      .col-lg-3.col-md-6.mb-4
+        .tarjeta-p-1.p-3.h-100
+          p Capacidad de caramelizar
+    
+    .row.justify-content-center
+      .col-lg-10
+        .fondo-color-sistema-p.p-4
+          .row 
+            .col-lg-9
+              p.mb-0.pt-lg-4 Algunos de los monosacáridos que tienen el grupo carbonilo libre pueden unirse con proteínas y ser responsables de colores oscuros (reacciones de pardeamiento). En algunos casos pueden encontrarse polioles derivados de hidrogenados de hexosas como sorbitol o manitol.
+            .col-lg-3.order-first.order-lg-last.mb-4.mb-lg-0
+              .bg-white.borde-8.p-4.h-100.align-content-center
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/41.svg",style="max-width: 173px;")
+    
+    .titulo-decorativo.my-4.mb-5
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Polisacáridos
+    
+    p.mb-4 Son compuestos complejos de peso molecular alto, son amorfos y no son dulces. Los almidones, las dextrinas, glicógeno y varios carbohidratos no digeribles son de interés nutricional.
+
+    .row.justify-content-center.mb-4
+      .col-lg-3.pe-lg-0
+        .h-100.align-content-center
+          figure
+            img.mx-auto(src="@/assets/curso/temas/42.png",style="max-width: 400px;")
+      .col-lg-5.ps-lg-0
+        .fondo-color-primario.h-100.align-content-center.p-4
+          p.mb-4.fw-bold Polisacáridos no digeribles
+          p.mb-0 Están compuestos por celulosa y hemicelulosa, de importancia en el estudio de la nutrición. Los rumiantes pueden utilizar la celulosa para producir energía porque tienen enzimas que les ayudan en este proceso. Para el ser humano, la celulosa es un constituyente dietético no digerible. Algunos polisacáridos no digeribles son de mucha utilidad en el procesamiento de los alimentos, como las pectinas que se encuentran en las frutas y que tienen la capacidad de absorber agua y formar geles.
+
+    .row.justify-content-center.mb-4
+      .col-lg-3.ps-lg-0.order-first.order-lg-last
+        .h-100.align-content-center
+          figure
+            img.mx-auto(src="@/assets/curso/temas/43.png",style="max-width: 400px;")
+      .col-lg-5.pe-lg-0
+        .fondo-color-primario.h-100.align-content-center.p-4
+          p.mb-4.fw-bold Funciones
+          p.mb-0 Casi todos los carbohidratos de la dieta se utilizan para suplir las necesidades energéticas del cuerpo. Una fracción muy pequeña de los carbohidratos disponibles se emplea para la síntesis de varios compuestos reguladores.
+
+    .row.justify-content-center.mb-4
+      .col-lg-3.pe-lg-0
+        .h-100.align-content-center
+          figure
+            img.mx-auto(src="@/assets/curso/temas/44.png",style="max-width: 400px;")
+      .col-lg-5.ps-lg-0
+        .fondo-color-primario.h-100.align-content-center.p-4
+          p.mb-4.fw-bold Energía
+          p.mb-0 #[b Al oxidarse], cada gramo de carbohidratos produce un promedio de #[b cuatro calorías]. Parte de los carbohidratos en forma de glucosa se usan directamente para cubrir las necesidades energéticas de tejidos inmediatos, unas pocas se almacenarán como glicógeno en el hígado y los músculos, y otra parte lo hará como tejido adiposo para convertir energía cuando sea necesario. La glucosa es la única forma de energía para #[b el cerebro y el sistema nervioso].
+    
+    Separador 
+
+    #t_3_2.titulo-segundo.color-acento-contenido
+      h2 3.2 Lípidos
+    
+    p.mb-4 Una de las funciones más importantes de los lípidos es servir de #[b fuente de energía metabólica y aportar elementos estructurales para la membrana celular]. También sirven como transportadores de las vitaminas liposolubles y precursores de hormonas. Los lípidos en forma de triglicéridos son una fuente importante para el almacenamiento de energía en el organismo (Rodríguez & Magro, 2008).
+
+    p.mb-5 Los lípidos se clasifican en tres grupos:
+
 
 
 </template>
